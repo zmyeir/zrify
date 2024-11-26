8d1db87574474d1b152249af4d011ae87e128337
c3bf26 <x@zrlab.org>
2024-11-26 19:11:06 +0800

    add: colemak

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ZR's Rime Input For You
+
+colemak 双拼
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/zmyeir/zrify/refs/heads/main/tools/src/colemak_key.svg">
+</p>
---
+++ tools/src/colemak_pinyn_map.js
@@ -0,0 +1,20 @@
+export const SHENGMU_MAP = {
+    b: 'b', p: 'p', m: 'm', f: 'f', d: 'd', t: 't', n: 'n', l: 'l',
+    g: 'g', k: 'k', h: 'h', j: 'j', q: 'q', x: 'x', r: 'r',
+    z: 'z', c: 'c', s: 's', zh: 'v', ch: 'u', sh: 'i', y: 'y', w: 'w'
+};
+
+export const YUNMU_MAP = {
+    a: 'a', o: 'o', e: 'e', i: 'i', u: 'u', v: 'v',
+    ai: 'c', ei: 'w', ui: 'v', ao: 'd', ou: 's', iu: 'q',
+    ie: 'm', ve: 'f', ue: 'f', an: 'n', en: 't', in: 'x',
+    un: 'z', ang: 'l', eng: 'p', ing: 'r', ong: 'y',
+    ia: 'b', iao: 'k', ian: 'j', iang: 'h', iong: 'y',
+    ua: 'b', uo: 'o', uai: 'r', uan: 'g', van: 'g', uang: 'h'
+};
+
+export const ZERO_SHENGMU_MAP = {
+    a: 'aa', ai: 'ai', an: 'an', ang: 'ah',
+    e: 'ee', ei: 'ei', en: 'en', eng: 'eg',
+    ao: 'ao', o: 'oo', ou: 'ou', er: 'er'
+};
